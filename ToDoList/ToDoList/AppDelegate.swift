a0cbfe19b392d1efdb654eda800e5dfc2b433bed
Dmitrii Sorochin <sorochin.dmitrii@gmail.com>
2023-05-02 17:03:26 +0300

    TASK001

---
+++ ToDoList/ToDoList/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToDoList
+//
+//  Created by Dmitrii Sorochin on 02.05.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let vc = ViewController()
+
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()    
+        return true
+    }
+}
+
---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Dmitrii Sorochin on 02.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
